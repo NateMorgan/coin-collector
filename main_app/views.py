a596bd7a03e809e4be7370ad44d15ddeff5a2643
Nate Morgan <nathanielrmorgan@gmail.com>
2022-11-06 12:27:30 -0500

    ignore data files

--- main_app/views.py
+++ main_app/views.py
@@ -6,4 +6,4 @@ def home(request):
   return HttpResponse('<h1>Coin Collector Home</h1>')
 
 def about(request):
-  return HttpResponse('<h1>About Coin Collector</h1>')
+  return render(request, 'about.html')
