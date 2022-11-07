356f25752626b50aa6edd35b476cd17982c6541f
Nate Morgan <nathanielrmorgan@gmail.com>
2022-11-07 16:47:36 -0500

    stubb up index page for collections

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Collection:
+  def __init__(self, name, description):
+    self.name = name
+    self.description = description
+
+collections = [
+  Collection('Buffalo Nickles',"Bag O' Buffalo Nickles"),
+  Collection('Morgan Silver Dollars',"Prize Possesstion of my Family"),
+  Collection('Big Fricken Coins',"Not only are they big, they are also heavy!")
+
+]
+
+
+
 
 def home(request):
-  return HttpResponse('<h1>Coin Collector Home</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def collections_index(request):
+  return render(request, 'collections/index.html', {'collections': collections})
